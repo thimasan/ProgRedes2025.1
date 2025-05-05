cbf0777f760be1739f2968e9c9c83e7a8c1ca93a
thimasan <thyagus.santana@gmail.com>
2025-05-05 13:35:51 -0300

    aquisição de dados q1

---
+++ q1.py
@@ -0,0 +1,9 @@
+
+
+
+# AQUISIÇÃO DE DADOS (IP E MÁSCARA)
+strIp = input("Informe o numqero de IP: ")
+intMask = int(input("Informe a máscara de sub-rede (CIDR = 18): "))
+
+
+hostsBits = 32 - intMask
