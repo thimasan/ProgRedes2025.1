672ce2cce0bc0c5dfdad85594dd6b98338a57765
thimasan <thyagus.santana@gmail.com>
2025-05-06 00:01:48 -0300

    parcial q1

--- q1.py
+++ q1.py
@@ -2,8 +2,19 @@
 
 
 # AQUISIÇÃO DE DADOS (IP E MÁSCARA)
-strIp = input("Informe o numqero de IP: ")
+strIp = input("Informe o numero de IP: ")
 intMask = int(input("Informe a máscara de sub-rede (CIDR = 18): "))
 
-
+# CÁLCULO DO NÚMERO DE HOSTS
 hostsBits = 32 - intMask
+
+# CÁLCULO DO NÚMERO DE SUB-REDES
+
+# CÁLCULO DO NÚMERO DE HOSTS
+hosts_Net = (2 ** hostsBits)- 2
+print("Número de hosts: ", hosts_Net)
+
+ip = 0
+for num in strIp.split('.'):
+    ip = (ip << 8) | int(num)
+print("IP: ", ip)
