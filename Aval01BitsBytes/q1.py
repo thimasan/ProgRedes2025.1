b20f8b5125d9af0049e75b3d36826aaa0204a749
thimasan <thyagus.santana@gmail.com>
2025-05-10 17:19:40 -0300

    refatoração do código usando funções

--- Aval01BitsBytes/q1.py
+++ Aval01BitsBytes/q1.py
@@ -5,17 +5,3 @@
 strIp = input("Informe o numero de IP: ")
 intMask = int(input("Informe a máscara de sub-rede (CIDR = 18): "))
 
-# CÁLCULO DO NÚMERO DE HOSTS
-hostsBits = 32 - intMask
-
-# CÁLCULO DO NÚMERO DE SUB-REDES
-
-# CÁLCULO DO NÚMERO DE HOSTS
-hosts_Net = (2 ** hostsBits)- 2
-print("Número de hosts: ", hosts_Net)
-
-ip = 0
-for num in strIp.split('.'):
-    ip = (ip << 8) | int(num)
-    print(ip)
-print("IP: ", ip)
