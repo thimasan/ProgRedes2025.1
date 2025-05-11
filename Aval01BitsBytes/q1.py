3315c71fd8ebf3511d690067e25dd14d7b17a277
thimasan <thyagus.santana@gmail.com>
2025-05-10 21:49:01 -0300

    Conversão do ip inteiro para string

--- Aval01BitsBytes/q1.py
+++ Aval01BitsBytes/q1.py
@@ -13,3 +13,11 @@ def ip_str_to_int(ip_str): # Converte o IP em string para inteiro
         ip_int = (ip_int << 8) | int(octeto) # Converte cada octeto para inteiro e desloca 8 bits para a esquerda
     return ip_int
 
+# CONVERSÃO DO IP_INTEIRO PARA STRING
+def int_to_ip_str(ip_int): #Converte o número inteiro de volta para uma string no formato xxxx.xxxx.xxxx.xxxx
+    octetos = []            #lista para armazenar os octetos
+    for i in range(4):
+        octetos.insert(0, str(ip_int & 0xFF)) # Extrai o último octeto
+        ip_int = ip_int >> 8 # Desloca 8 bits para a direita
+    return '.'.join(octetos)
+
